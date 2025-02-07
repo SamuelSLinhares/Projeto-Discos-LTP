300009943ea2a57c28024c93758305793167832b
oliverodrigues <rodrigues.luzia@academico.ifpb.edu.br>
2025-02-06 22:46:37 -0300

    versao melhorada (erro input Senha

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-
 function capturardados(event) {
     event.preventDefault();
-    var cpf = document.getElementById("cpf").value
-    var senha = document.getElementById("Senha").value
-    
-    console.log("CPF:", cpf)
+
+    const cpfInput = document.getElementById("cpf");
+    const senhaInput = document.getElementById("Senha");
+
+    if (!cpfInput) {
+        console.error("Error: CPF input element not found.");
+        return;
+    }
+    if (!senhaInput) {
+      console.error("Error: Senha input element not found.");
+      return;
+    }
+    const cpf = cpfInput.value;
+    const senha = senhaInput.value;
+
+    console.log("CPF:", cpf);
     console.log("Senha:", senha);
 }
